20dc5525dd0bf7c3b99c83440551a126ec678fce
Joseph Popoola <joseph.popoola@willmottdixon.co.uk>
2016-12-16 10:45:20 +0000

    creating services and using them to retrieve data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from 'angular2/core';
 import {CourseComponent} from './courses/courses.component'
+import {AuthorComponent} from './authors/authors.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular</h1><courses></courses>',
-    directives: [CourseComponent]
+    template: '<h1>Angular</h1><courses></courses><authors></authors>',
+    directives: [CourseComponent, AuthorComponent]
 })
 export class AppComponent { }
